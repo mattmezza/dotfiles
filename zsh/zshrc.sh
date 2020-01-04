465856f230762fa3ff9a3d877725ecc0895aa7b6
Matteo Merola <mattmezza@gmail.com>
2020-01-04 23:49:47 +0100

    Added export for poetry (python)

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -138,6 +138,9 @@ if command -v pyenv 1>/dev/null 2>&1; then
  eval "$(pyenv virtualenv-init -)"
 fi
 
+# poetry
+export PATH=$PATH:$HOME/.poetry/bin
+
 # pip
 export PIP_REQUIRE_VIRTUALENV=true
 # cause sometimes you need to do this...
