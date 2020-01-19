b33626cd5736fea800ee84354f5ebeae1ea6ca45
Matteo Merola <mattmezza@gmail.com>
2020-01-19 17:01:24 +0100

    Fixed plugin install system.

--- plugins.sh
+++ plugins.sh
@@ -3,17 +3,19 @@ plugin_install_single() {
 	[[ -z "$2" ]] && { return 1; }
 	[[ -z "$3" ]] && { return 1; }
 	if [ -d "$3" ]; then
-		echo "Uninstalling $3:$1"
+		echo ""
 		rm -rf "$3"
+		echo "Uninstalled '$3'"
 		mkdir "$3"
-		echo "Installing $3:$1"
-		git clone -q --branch $1 $2 $3
-		echo "Installed $3:$1"
+		echo "Installing '$3'..."
+		git clone -q --branch $1 $2 $3 > /dev/null 2> /dev/null
+		echo "Installed '$3:$1'"
 	else
+		echo "$3 is not a directory"
 		return 1
 	fi
 }
 
 plugin_install() {
-	cat plugins.txt | xargs -L1 -I {} -P4 echo "plugin_install_single {}"
+	source <(cat plugins.txt | while read in; do echo "plugin_install_single $in"; done)
 }
