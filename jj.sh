28bb55edda7fbb4e6ebde81bbed4f0ffe43fbad8
Matteo Merola <mattmezza@gmail.com>
2020-04-21 12:05:37 +0200

    Improved jj with -h -d -a commands.

--- jj.sh
+++ jj.sh
@@ -1,33 +1,53 @@
 jj() {
-    DB=${JJDB:-"$HOME/jj.txt"}
+    DB=${JJDB:-"$HOME/.jj"}
     if [ $# -eq 0 ]
     then
         cd $HOME
         return
     elif [ $# -eq 1 ]
     then
-        cd $(cat $DB | grep -E "^$1" | sed -E 's/^.*\:(.*)$/\1/')
-        return
+        if [[ $1 == -* ]]
+        then
+            # skip and goto help
+        else
+            cd $(cat $DB | grep -Em1 "^$1" | sed -E 's/^.*\:(.*)$/\1/')
+            return
+        fi
     else
-        case $1 in
-            -a)
-                if [ $# -eq 3]
+        case "$1" in
+            "-a")
+                if [ $# -eq 3 ]
                 then
                     echo "$2:$3" >> $DB
+                    return
+                fi
+                ;;
+            "-d")
+                if [ $# -eq 2 ]
+                then
+                    sed -Ei '' "/^$2\:.*$/d" $DB
+                    return
+                fi
+                ;;
+            "-r")
+                if [ $# -eq 2 ]
+                then
+                    cat $DB | grep -Em1 "^$2" | sed -E 's/^.*\:(.*)$/\1/'
                     return
                 fi
                 ;;
             *)
                 ;;
         esac
-        return
     fi
 
     cat <<- EOF
 Usage:
-    $ $0 dev
-    $ $0 -a name path
+    $ $0 name         # to cd directly into 'name'
+    $ $0 -r name      # to resolve 'name'
+    $ $0 -a name path # to add 'name' as 'path'
+    $ $0 -d name      # to delete 'name'
 
-DB is at $DB.
+DB is at '$DB'.
 EOF
 }
