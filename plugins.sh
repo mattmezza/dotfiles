76a8134eab674147c8d7443266df6346bdbfe579
Matteo Merola <mattmezza@gmail.com>
2020-04-26 15:48:13 +0200

    Improved plugins install.

--- plugins.sh
+++ plugins.sh
@@ -1,19 +1,18 @@
 plugin_install_single() {
-    [[ -z "$1" ]] && { return 1; }
-    [[ -z "$2" ]] && { return 1; }
-    [[ -z "$3" ]] && { return 1; }
-    if [ -d "$3" ]; then
-        echo ""
-        rm -rf "$3"
+    [[ -z "$1" ]] && { return 1 }
+    [[ -z "$2" ]] && { return 1 }
+    [[ -z "$DOT/$3" ]] && { return 1 }
+
+    if [ -d "$DOT/$3" ]; then
+        rm -rf "$DOT/$3"
         echo "Uninstalled '$3'"
-        mkdir "$3"
-        echo "Installing '$3'..."
-        git clone -q --branch $1 $2 $3 > /dev/null 2> /dev/null
-        echo "Installed '$3:$1'"
-    else
-        echo "$3 is not a directory"
-        return 1
     fi
+
+    echo "Installing '$3'..."
+    mkdir -p "$DOT/$3"
+    git clone -q --branch $1 $2 "$DOT/$3" > /dev/null 2> /dev/null
+    echo "Installed '$3:$1'"
+    return 0
 }
 
 plugin_install() {
