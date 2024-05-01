2155feee10bd515288908d48457a6c93df6a2d5c
Matteo Merola <mattmezza@gmail.com>
2024-05-01 11:46:23 +0200

    fugitive

--- nvim/lua/mattmezza/lazy/fugitive.lua
+++ nvim/lua/mattmezza/lazy/fugitive.lua
@@ -16,18 +16,18 @@ return {
 
                 local bufnr = vim.api.nvim_get_current_buf()
                 local opts = {buffer = bufnr, remap = false}
-                vim.keymap.set("n", "<leader>p", function()
-                    vim.cmd.Git('push')
+                vim.keymap.set("n", "<leader>gp", function()
+                    vim.cmd.Git("push")
                 end, opts)
 
                 -- rebase always
-                vim.keymap.set("n", "<leader>P", function()
-                    vim.cmd.Git({'pull',  '--rebase'})
+                vim.keymap.set("n", "<leader>gP", function()
+                    vim.cmd.Git("pull --rebase")
                 end, opts)
 
-                -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-                -- needed if i did not set the branch up correctly
-                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+                -- NOTE: It allows me to easily set the branch i am pushing any
+                -- tracking needed if i did not set the branch up correctly.
+                vim.keymap.set("n", "<leader>gt", ":Git push -u origin ", opts);
             end,
         })
 
