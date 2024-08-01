1b7257a3e33c4b90882ab61393ba2b86186ee911
Matteo Merola <mattmezza@gmail.com>
2024-08-01 17:22:03 +0200

    removed superfluous ensure installed

--- nvim/lua/mattmezza/lazy/lsp.lua
+++ nvim/lua/mattmezza/lazy/lsp.lua
@@ -27,8 +27,6 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "rust_analyzer",
-                "tsserver",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
