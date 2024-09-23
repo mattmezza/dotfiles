99fecfee4422abbdf2e635c8d95250e8a7c2898a
Matteo Merola <mattmezza@gmail.com>
2024-09-23 14:56:34 +0200

    added plugin for todo comments

---
+++ nvim/lua/mattmezza/lazy/todo.lua
@@ -0,0 +1,11 @@
+return {
+	"folke/todo-comments.nvim",
+	event = "VeryLazy",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local todo = require("todo-comments")
+		todo.setup({})
+
+		vim.keymap.set("n", "<leader>st", ":TodoTrouble<CR>", { desc = "[S]how [T]odos" })
+	end,
+}
