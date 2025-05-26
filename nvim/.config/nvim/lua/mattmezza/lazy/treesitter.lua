1d9366191772da9a33e7e8379384c02f1d1d0fcf
Matteo Merola <mattmezza@gmail.com>
2025-05-26 11:25:20 +0200

    upgraded nvim plugins

--- nvim/.config/nvim/lua/mattmezza/lazy/treesitter.lua
+++ nvim/.config/nvim/lua/mattmezza/lazy/treesitter.lua
@@ -6,19 +6,19 @@ return {
             -- A list of parser names, or "all"
             ensure_installed = {
                 "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash",
+                "jsdoc", "bash", "go",
             },
-
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
 
             -- Automatically install missing parsers when entering buffer
             -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-            auto_install = true,
+            auto_install = false,
 
             indent = {
                 enable = true
             },
+    --[[
 
             highlight = {
                 -- `false` will disable the whole extension
@@ -30,8 +30,8 @@ return {
                 -- Instead of true it can also be a list of languages
                 additional_vim_regex_highlighting = { "markdown" },
             },
-        })
-
+    ]]
+})
         local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
         treesitter_parser_config.templ = {
             install_info = {
