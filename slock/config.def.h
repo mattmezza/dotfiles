c81f024872980429bf6e50d8e0382bc988ecd05b
Matteo Merola <mattmezza@gmail.com>
2025-07-22 15:22:33 +0200

    added suckless slock (with patches)

---
+++ slock/config.def.h
@@ -0,0 +1,22 @@
+/* user and group to drop privileges to */
+static const char *user  = "matteo";
+static const char *group = "matteo";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[INPUT_ALT] = "#227799", /* during input, second color */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "6x13";
